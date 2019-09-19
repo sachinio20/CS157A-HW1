d09bba5d1f1fe56b9c85414e26df345a657fa48a
Sachin <sachin.shah@sjsu.edu>
2019-09-19 12:33:27 -0700

    added build files

---
+++ src/hw1Actual/MysqlCon.java
@@ -0,0 +1,28 @@
+package hw1Actual;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+public class MysqlCon {
+	public static void main(String [] args) {
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+			
+			Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/cs157a", "root", "#Indianboy12");
+			Statement stmt = con.createStatement();
+			ResultSet rs = stmt.executeQuery("select * from emp");
+			while(rs.next()) {
+				System.out.println(rs.getInt(1)+ " " + rs.getString(2) + " "+ rs.getInt(3));
+				
+			}
+			con.close();
+		}
+		catch(Exception ex) {
+			System.out.println(ex);
+		}
+
+	}
+
+}
